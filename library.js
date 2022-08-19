74f89f0af3c98a75264cdee931fc9a9b5896ded2
RJaime-jr <Ricoa.jaime@outlook.com>
2022-08-18 21:22:37 -0700

    made add book button to pull up the form. Also tested the creating card function. Need to now make the info inputted in the form usable

--- library.js
+++ library.js
@@ -1,4 +1,7 @@
-let testBut = document.getElementById('test');
+const addBut = document.getElementById('addBtn');
+const bookGrid = document.getElementById('bookDisplay');
+const active1 = document.getElementById('form1');
+const active2 = document.getElementById('form2');
 
 
 let myLibrary = [];
@@ -27,7 +30,7 @@ function addBookToLibrary() {
 }
 
 let animal = new Book("Zoo", "Rico Jaime", 200, true);
-
+/*
 console.log(animal.title);
 
 console.log(animal.author);
@@ -36,10 +39,11 @@ console.log(animal.pages);
 console.log(animal.read);
 console.log(animal.info);
 //console.log(animal.info);
-
+*/
 
 const createBookCard = (book) => {
     const bookCard = document.createElement('div');
+    bookCard.className = "book";
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p')
@@ -50,11 +54,30 @@ const createBookCard = (book) => {
 
     title.textContent = `"${book.title}"`;
     author.textContent = book.author;
-    pages.textContent = `${book.pages} pages`
+    pages.textContent = `${book.pages} pages`;
+
+
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    bookGrid.appendChild(bookCard);
+
 }
 
-testBut.addEventListener('click', function () {
-    createBookCard();
-    console.log("butt");
+addBut.addEventListener('click', function () {
+
+    //createBookCard(animal);
+
+    active1.classList.add("active");
+    active2.classList.add("active");
+
+    console.log("A");
+
+
     return;
 })
+
+/*
+active1.classList.remove("active");
+        active2.classList.remove("active");
+*/
