be20f64ccb9e0c568882ccff6f2d8d1176d4db6c
RJaime-jr <Ricoa.jaime@outlook.com>
2022-08-22 16:19:28 -0700

    Project complete. Few bugs but overall task complete!

--- library.js
+++ library.js
@@ -4,67 +4,99 @@ const form = document.getElementById('form-data');
 const overlay = document.getElementById('overlay');
 let bookCardCount = 0;
 
+class Library {
+    constructor() {
+        this.books = []
+    }
 
+    removeBook(title) {
+        this.books = this.books.filter((book) => book.title !== title)
+    }
+}
 
-let animal = new Book("Zoo", "Rico Jaime", 200, true);
+const myLibrary = new Library;
 
-
-let myLibrary = [];
-
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(
+        title = 'empty',
+        author = 'empty',
+        pages = '0',
+        isRead = false
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 }
 
 
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    myLibrary.books.push(book);
 }
 
 function showLibrary() {
-    createBookCard(myLibrary[bookCardCount]);
+    createBookCard(myLibrary.books[bookCardCount]);
     bookCardCount++;
 
 
 }
 
-
+//Create Book card
 const createBookCard = (book) => {
+    //Create elements for bookcard
     const bookCard = document.createElement('div');
-    bookCard.className = "book";
     const title = document.createElement('p');
     const author = document.createElement('p');
-    const pages = document.createElement('p')
+    const pages = document.createElement('p');
     const buttonGroup = document.createElement('div')
-    const readBtn = document.createElement('button')
-    const removeBtn = document.createElement('button')
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
 
+    //Assign Classes for bookcard
+    bookCard.className = "book";
+    buttonGroup.classList.add('card-btn');
+    readBtn.classList.add('btn');
+    removeBtn.classList.add('btn');
 
+    //Text Content of elements
     title.textContent = `"${book.title}"`;
     author.textContent = book.author;
     pages.textContent = `${book.pages} pages`;
+    removeBtn.textContent = "Remove"
 
+    //Color and text of Read logic
+    if (book.isRead) {
+        readBtn.textContent = "Read";
+        readBtn.classList.add('color-green');
+    }
+    else {
+        readBtn.textContent = "Read";
+        readBtn.classList.add('color-red');
+    }
 
-    bookCard.appendChild(title)
-    bookCard.appendChild(author)
-    bookCard.appendChild(pages)
+    //Button Click events
+    readBtn.onclick = toggleRead;
+    removeBtn.onclick = removeBook;
+    //removeBtn.onclick = updateBooksGrid;
+
+    //Putting all the elements together
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(buttonGroup);
+    buttonGroup.appendChild(readBtn);
+    buttonGroup.appendChild(removeBtn);
     bookGrid.appendChild(bookCard);
-
 }
 
-
+//Form open to submit data
 function openBookForm() {
     form.classList.add("active");
     overlay.classList.add("active");
 
     console.log("A");
-    /*
-    addBookToLibrary();
-    showLibrary();
-    */
     console.log(myLibrary.length);
 }
 
@@ -88,15 +120,40 @@ form.addEventListener('submit', cardFromForm);
 
 function cardFromForm(event) {
     event.preventDefault();
-
-    const myFormData = new FormData(event.target);
-
-    const formDataObj = Object.fromEntries(myFormData.entries());
-    //let book = new Book(formDataObj);
-
-    addBookToLibrary(formDataObj);
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    let isRead = document.getElementById('isRead').checked;
+    let book = new Book(title, author, pages, isRead);
+    addBookToLibrary(book);
     showLibrary();
-
-    console.log(formDataObj);
 };
 
+
+const toggleRead = (e) => {
+    e.target.classList.toggle('color-green');
+    e.target.classList.toggle('color-red');
+
+    console.log(e.target.classList);
+}
+
+const removeBook = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+    myLibrary.removeBook(title)
+    updateBooksGrid()
+    console.log(myLibrary);
+}
+
+const updateBooksGrid = () => {
+    resetBooksGrid()
+    for (let book of myLibrary.books) {
+        createBookCard(book)
+    }
+}
+
+const resetBooksGrid = () => {
+    bookGrid.innerHTML = ''
+}
